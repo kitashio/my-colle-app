cf71d74306eb6c07c11ffc4455561854dc5d95dc
kitashio <kitashio.pr@gmail.com>
2021-11-10 23:51:43 +0900

    UIアプデ

--- lib/Top_Page/top_user_page.dart
+++ lib/Top_Page/top_user_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:myfirstapp/Top_Page/user_add_page.dart';
 import 'package:myfirstapp/Top_Page/user_login_page.dart';
 
@@ -16,12 +17,37 @@ class _TopPageState extends State<TopUserPage> {
     return Scaffold(
       body: Center(
         child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(color: Color.fromRGBO(255, 243, 228, 100)),
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(30,500,30,0),
+            padding: const EdgeInsets.fromLTRB(30,300,30,0),
             child: ListView(
               children: [
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: GoogleFonts.raleway(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Welcome  To  ',
+                      ),
+                      TextSpan(
+                        text: 'My COLLE',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(61, 86, 178, 100),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 200),
                 ElevatedButton(
-                  child: Text('アカウント登録'),
+                  child: Text('アカ登録'),
                   style: ElevatedButton.styleFrom(
                     shape: const StadiumBorder(),
                     padding: EdgeInsets.fromLTRB(15, 5, 15, 8),
