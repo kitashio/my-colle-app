aa2d805b6e9ba8edccf81238a8728e00a3207a64
kitashio <kitashio.pr@gmail.com>
2021-12-23 01:24:55 +0900

    コレクションページ完成！

---
+++ lib/model/my_colle_add_model.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CollectionAddModel with ChangeNotifier {
+  String title;
+  String describe;
+
+  Future addItem () async {
+    if (title == null) {
+      throw 'タイトルが入力されていません。';
+    }
+
+    if (describe == null) {
+      throw '説明文が入力されていません。';
+    }
+
+    await FirebaseFirestore.instance.collection('collection') // コレクションID指定
+        .doc('id_001') // ドキュメントID自動生成(collection)
+        .collection('items')
+        .add({
+      'title': title,
+      'describe': describe,
+    });
+  }
+
+}
---
+++ lib/model/my_colle_page_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import '../Items.dart';
+
+class CollectionPageModel with ChangeNotifier {
+
+  List<Items> items;
+
+  Future fetchData () async {
+    final QuerySnapshot snapshot =  await FirebaseFirestore.instance
+        .collection('collection')
+        .doc('id_001')
+        .collection('items')
+        .get();
+
+    final List<Items> items = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String title = data['title'];
+      final String describe = data['describe'];
+      return Items(title, describe);
+    }).toList();
+
+    this.items = items;
+    notifyListeners();
+  }
+
+}
